1de465fb5cdac761afad9b8a89c7b413cf620a86
xubiao <318060603@qq.com>
2020-01-02 10:01:30 +0800

    第一次提交代码库

---
+++ src/main/java/com/accp/demo/DzwApplication.java
@@ -0,0 +1,13 @@
+package com.accp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DzwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DzwApplication.class, args);
+	}
+
+}
