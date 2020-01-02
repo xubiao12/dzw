f6fa7e369bfb6956825eb54258e87a51222fe511
xubiao <318060603@qq.com>
2020-01-02 10:03:31 +0800

    第一次修改

--- src/main/java/com/accp/demo/DzwApplication.java
+++ src/main/java/com/accp/demo/DzwApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DzwApplication {
 
 	public static void main(String[] args) {
+		System.out.println("第一次修改");
 		SpringApplication.run(DzwApplication.class, args);
 	}
 
