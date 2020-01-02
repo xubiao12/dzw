cb4ef29f1b6cdd52d64f97004d0d54153d57c238
xubiao <318060603@qq.com>
2020-01-02 10:09:14 +0800

    diercixiugai

--- src/main/java/com/accp/demo/DzwApplication.java
+++ src/main/java/com/accp/demo/DzwApplication.java
@@ -8,6 +8,7 @@ public class DzwApplication {
 
 	public static void main(String[] args) {
 		System.out.println("第一次修改");
+		System.out.println("第二次修改");
 		SpringApplication.run(DzwApplication.class, args);
 	}
 
